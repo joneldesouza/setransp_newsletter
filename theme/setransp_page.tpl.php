a832390fb20a9ef9e638cc54dc13cfa1e8138754
Jonel de Souza Nogueira <joneldesouza@gmail.com>
2014-07-30 11:35:20 -0300

    Update

--- theme/setransp_page.tpl.php
+++ theme/setransp_page.tpl.php
@@ -9,9 +9,10 @@ $articles = $node->field_reference_articles['und'];
   <h1><?php print $node->title; ?></h1>
 
   <?php foreach ($articles as $item) : ?>
-    <a href="/node/<?php print($item['entity']->nid); ?>">
+    <img src="<?php print image_style_url('medium', $item['entity']->field_image['und'][0]['uri']); ?>" alt="<?php print($item['entity']->title); ?>" />
+    <h2><a href="/node/<?php print($item['entity']->nid); ?>">
       <?php print($item['entity']->title); ?>
-    </a>
+    </a></h2>
     <?php print $item['entity']->body['und'][0]['value']; ?>
   <?php endforeach; ?>
 
